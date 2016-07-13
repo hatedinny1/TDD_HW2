8939e07dcdc786eaf18c41badab09810bec30947
powen <powen@DESKTOP-RO931TV>
2016-07-13 11:08:11 +0800

    第一個紅燈:第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PotterShoppingCart;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class ShoppingCartTest
+    {
+        [TestMethod]
+        public void Test_PotterOne_1_AcualPaid_Should_be_100()
+        {
+            //arrange
+            var books = new Dictionary<string, int> {
+                { "episode 1",1}
+            };
+            var cart = new ShoppingCart();
+            //act
+            cart.CalculateActualPaid(books);
+            //assert
+            var excepted = 100;
+            Assert.AreEqual(excepted, cart.actualPaid);
+        }
+    }
+}
---
+++ ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        public double actualPaid { get; set; }
+
+        public void CalculateActualPaid(Dictionary<string, int> books)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
