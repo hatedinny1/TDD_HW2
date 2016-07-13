1d2f827c7806a79bd52bd3dbe657e8e32ae58664
powen <powen@DESKTOP-RO931TV>
2016-07-13 11:28:55 +0800

    第四個紅燈:一二三四集各買了一本，價格應為100*4*0.8=320

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using PotterShoppingCart;
 
 namespace PotterShoppingCart.Tests
 {
@@ -53,6 +51,24 @@ namespace PotterShoppingCart.Tests
             cart.CalculateActualPaid(books);
             //assert
             var excepted = 270;
+            Assert.AreEqual(excepted, cart.actualPaid);
+        }
+
+        [TestMethod()]
+        public void Test_PotterOne_1_PotterTwo_1_PotterThree_1_PotterFour_1_AcualPaid_Should_be_320()
+        {
+            //arrange
+            var books = new Dictionary<string, int> {
+                { "episode 1",1},
+                { "episode 2",1},
+                { "episode 3",1},
+                { "episode 4",1},
+            };
+            var cart = new ShoppingCart();
+            //act
+            cart.CalculateActualPaid(books);
+            //assert
+            var excepted = 320;
             Assert.AreEqual(excepted, cart.actualPaid);
         }
     }
