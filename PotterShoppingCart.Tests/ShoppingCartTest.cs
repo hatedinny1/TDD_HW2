3262d1f76bb8df37d8cc9e98e8b8351db049b939
powen <powen@DESKTOP-RO931TV>
2016-07-13 11:54:21 +0800

    第六個紅燈:一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -1,5 +1,4 @@
-﻿using PotterShoppingCart;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
 namespace PotterShoppingCart.Tests
@@ -83,13 +82,29 @@ namespace PotterShoppingCart.Tests
                 { "episode 3",1},
                 { "episode 4",1},
                 { "episode 5",1},
-
             };
             var cart = new ShoppingCart();
             //act
             cart.CalculateActualPaid(books);
             //assert
             var excepted = 375;
+            Assert.AreEqual(excepted, cart.actualPaid);
+        }
+
+        [TestMethod()]
+        public void Test_PotterOne_1_PotterTwo_1_PotterThree_2_AcualPaid_Should_be_370()
+        {
+            //arrange
+            var books = new Dictionary<string, int> {
+                { "episode 1",1},
+                { "episode 2",1},
+                { "episode 3",2},
+            };
+            var cart = new ShoppingCart();
+            //act
+            cart.CalculateActualPaid(books);
+            //assert
+            var excepted = 370;
             Assert.AreEqual(excepted, cart.actualPaid);
         }
     }
