658d3b97e71a79ef6184bc7f3128465e2ce23bd9
powen <powen@DESKTOP-RO931TV>
2016-07-13 11:51:01 +0800

    第五個綠燈:重購if-else流程，改使用Dictionary對照

--- ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart.cs
@@ -1,35 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace PotterShoppingCart
 {
     public class ShoppingCart
     {
-        public double actualPaid { get; set; }
+        public Dictionary<int, double> discountComparison { get; private set; }
 
+        public ShoppingCart()
+        {
+            this.discountComparison = new Dictionary<int, double> {
+                { 1,1},
+                { 2,0.95},
+                { 3,0.9},
+                { 4,0.8},
+                { 5,0.75},
+            };
+        }
+
+        public double actualPaid { get; set; }
         private const int bookPrice = 100;
+
         public void CalculateActualPaid(Dictionary<string, int> books)
         {
             var booksCount = books.Sum(x => x.Value);
             var shouldPaid = booksCount * bookPrice;
-            if (booksCount == 1)
-            {
-                this.actualPaid = shouldPaid;
-            }
-            else if (booksCount == 2)
-            {
-                this.actualPaid = shouldPaid * 0.95;
-            }
-            else if (booksCount == 3)
-            {
-                this.actualPaid = shouldPaid * 0.9;
-            }
-            else if (booksCount == 4)
-            {
-                this.actualPaid = shouldPaid * 0.8;
-            }
+            this.actualPaid = shouldPaid * this.discountComparison[booksCount];
         }
     }
 }
